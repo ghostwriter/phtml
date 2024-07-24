9a001f7cbb3921b4017e6d002b4d2abcaf155a4f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-23 23:04:14 -0500

    Update PhtmlTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PhtmlTest.php
+++ tests/Unit/PhtmlTest.php
@@ -6,16 +6,32 @@ namespace Tests\Unit;
 
 use Ghostwriter\Phtml\Interface\PhtmlExceptionInterface;
 use Ghostwriter\Phtml\Interface\PhtmlInterface;
+use Ghostwriter\Phtml\Path\CachePath;
+use Ghostwriter\Phtml\Path\TemplatePath;
 use Ghostwriter\Phtml\Phtml;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 use Throwable;
 
 use function is_a;
+use function sys_get_temp_dir;
 
 #[CoversClass(Phtml::class)]
+#[UsesClass(TemplatePath::class)]
+#[UsesClass(CachePath::class)]
 final class PhtmlTest extends TestCase
 {
+    /**
+     * @throws Throwable
+     */
+    public function testCachePathToString(): void
+    {
+        $phtmlDirectory = sys_get_temp_dir();
+
+        self::assertSame($phtmlDirectory, Phtml::new($phtmlDirectory, $phtmlDirectory)->cachePath()->toString());
+    }
+
     /**
      * @throws Throwable
      */
@@ -23,5 +39,15 @@ final class PhtmlTest extends TestCase
     {
         self::assertTrue(is_a(Phtml::class, PhtmlInterface::class, true));
         self::assertTrue(is_a(PhtmlExceptionInterface::class, Throwable::class, true));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testTemplatePathToString(): void
+    {
+        $phtmlDirectory = sys_get_temp_dir();
+
+        self::assertSame($phtmlDirectory, Phtml::new($phtmlDirectory, $phtmlDirectory)->templatePath()->toString());
     }
 }
